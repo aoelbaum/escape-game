caeb1e6512df08c11aae979c7805d71049a93e02
aoelbaum <ashleyoelbaum@gmail.com>
2019-02-28 17:09:33 -0500

    Initial commit w/ hover hit box, placeholder scenes and graphics

---
+++ Assets/Scripts/Arrows.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Arrows : MonoBehaviour {
+
+    public Image arrowImage;
+	// Use this for initialization
+	void Start ()
+    {
+        arrowImage.enabled = false;
+	}
+		
+
+    void OnMouseOver()
+    {
+        arrowImage.enabled = true;
+        Debug.Log("hover on");
+        
+        
+        enabled = true;
+    }
+
+    void OnMouseExit()
+    {
+
+        arrowImage.enabled = false;
+
+
+        enabled = true;
+
+    }
+}
---
+++ Assets/Scripts/DoorSceneChange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorSceneChange : MonoBehaviour {
+
+    public int num;
+    
+   
+    
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+
+        
+    }
+
+    void OnMouseDown()
+    {
+        
+        SceneManager.LoadScene(num);
+        Debug.Log("Scene changed to" + num);
+        
+    }
+}
